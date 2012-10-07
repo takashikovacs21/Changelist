2b018bf472e3585ba673de6e1e843395c5055187
Kent Tam <ktam39@gmail.com>
2012-10-07 15:23:46 -0700

    Added Readme, License, and Github banner

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	<body>
+		<div id="banner"><a href="https://github.com/aspirationalvigilante/Changelist">Fork me on GitHub</a></div>
 		<h1>Project Bamboo Changelist Creator</h1>
 		<form action="parse.php" method="post" id="form">
 			<textarea rows="100" name="changelist">
